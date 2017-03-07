efabc700c0425b6d96413d73129a2ba7acfd84c7
Andrzej Micialkiewicz <andrzej.micialkiewicz@gmail.com>
2017-03-07 01:40:46 +0100

    basic functions, no tests

---
+++ rss_test/rss_list/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class RssListConfig(AppConfig):
+    name = 'rss_list'
---
+++ rss_test/rss_list/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.views.generic import ListView
+
+from rss_test.rss_list.models import Feed
+
+
+class RssListView(ListView):
+    model = Feed
+    template_name = 'rss_list/rss_list.html'
+    context_object_name = "rss_list"
+    paginate_by = 5
